d609a7c6b9f84714aa0b36bae3b04a04783e7522
Yashwanthss2954 <108980993+Yashwanthss2954@users.noreply.github.com>
2025-06-01 18:05:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -115,8 +115,7 @@ class App extends Component {
         const comment = this.state.python.split('#')[this.state.python.split('#').length - 1].split('\n')[0];
         console.log(comment);
         // post request
-        axios.post("https://5325-2406-b400-b1-7ad1-f8ea-5051-79fd-fd5.ngrok-free.app
-/codify", { text : comment })
+        axios.post("https://5325-2406-b400-b1-7ad1-f8ea-5051-79fd-fd5.ngrok-free.app/codify", { text : comment })
         .then(res => {
           console.log(res.data);
           if(res.data.code){
